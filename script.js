438eb1632e3f34857b015d31991595b7ccd43293
Natalya Myunster <natalyamyunster@yahoo.com>
2020-08-12 16:51:32 +0200

    added index, script, style.

---
+++ script.js
@@ -0,0 +1,35 @@
+/*let a = 1;
+a = 11;
+alert(a);*/
+
+/*let a = 578;
+let b = 875;
+alert(a + b);
+
+let c =  a + b;
+alert(c)*/
+
+/*let a = 15 + 9 * 9 / 3;
+alert(a);*/
+
+/* Tasks */
+
+/*let count = 700;
+alert(count);
+
+let  JS = 25;
+let programinglenguage = 55;
+alert(JS + programinglenguage);
+
+let points = 23;
+alert(points * points * points);*/
+
+
+
+const radius = 500;
+
+let area = Math.PI * radius * radius;
+alert(area);
+
+let circumference = 2 *  Math.PI * radius;
+alert(circumference);
